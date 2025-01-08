b14f45af4cf1a1f2a36289bd778f800f699e10de
junghwan <ssoola9@gmail.com>
2025-01-09 03:17:49 +0900

    tailwindcss 프레임워크 적용

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,8 +1,8 @@
 function App() {
   return (
-    <div className="">
-      첫번째 커밋
-    </div>
+      <div className="min-h-screen flex justify-center items-center">
+          <h1 className="text-3xl font-bold text-blue-600">React + Tailwind</h1>
+      </div>
   );
 }
 
